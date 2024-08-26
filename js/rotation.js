d660786378868cc1009f49bfcad3231b0ac31aab
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-08-26 09:39:59 -0300

    ajuste de estrutura

---
+++ js/rotation.js
@@ -0,0 +1,15 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    document.querySelector('.container').style.transform = 'rotateY(-90deg)';
+});
+
+document.getElementById('signupButton').addEventListener('click', function() {
+    document.querySelector('.container').style.transform = 'rotateY(-180deg)';
+});
+
+document.getElementById('signupLink').addEventListener('click', function() {
+    document.querySelector('.container').style.transform = 'rotateY(-180deg)';
+});
+
+document.getElementById('loginLink').addEventListener('click', function() {
+    document.querySelector('.container').style.transform = 'rotateY(-90deg)';
+});
